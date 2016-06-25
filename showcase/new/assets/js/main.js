111e8953d09597bd619044801503072c84a1c906
Suryabrata Dutta <suryabrata.dutta@yale.edu>
2016-06-25 23:27:48 +0200

    Removing Loading Icon

--- showcase/new/assets/js/main.js
+++ showcase/new/assets/js/main.js
@@ -6,11 +6,7 @@
 
 //paste this code under the head tag or in a separate js file.
 	// Wait for window load
-	$(window).load(function() {
-		// Animate loader off screen
-		$(".se-pre-con").fadeOut("slow");;
-	});
-
+	
 (function($) {
 
 	skel.breakpoints({
