ddcf7d28f1b88049e5216b1de04c7e45381fedc1
Suryabrata Dutta <suryabrata.dutta@yale.edu>
2016-10-02 14:34:35 -0400

    Fix Categories Formatting Issues

--- database/assets/js/index.js
+++ database/assets/js/index.js
@@ -1,19 +1,7 @@
 $(function() {
 	var $wrapper = $('#wrapper');
 
-  // theme switcher
-  var theme_match = String(window.location).match(/[?&]theme=([a-z0-9]+)/);
-  var theme = (theme_match && theme_match[1]) || 'default';
-  var themes = ['default','legacy','bootstrap2','bootstrap3'];
-  $('head').append('<link rel="stylesheet" href="assets/css/selectize.' + theme + '.css">');
-
-  var $themes = $('<div>').addClass('theme-selector').insertAfter('h1');
-  for (var i = 0; i < themes.length; i++) {
-    $themes.append('<a href="?theme=' + themes[i] + '"' + (themes[i] === theme ? ' class="active"' : '') + '>' + themes[i] + '</a>');
-  }
-
-
-	// display scripts on the page
+  	// display scripts on the page
 	$('script', $wrapper).each(function() {
 		var code = this.text;
 		if (code && code.length) {
