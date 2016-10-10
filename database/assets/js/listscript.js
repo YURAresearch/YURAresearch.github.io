72544adf273d2fe9dd4a62aa9833064a0b4a5c56
Nishant Jain <najain@users.noreply.github.com>
2016-10-09 20:33:12 -0400

    Update listscript.js

--- database/assets/js/listscript.js
+++ database/assets/js/listscript.js
@@ -1,5 +1,30 @@
 /// *** Initialize List *** (using List.js)
 
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+var tech = getUrlParameter('ticket');
+if(tech != undefined)
+{
+  
+}
+else
+{
+  $("#rdbcontent").hide();
+}
+
 // Pagination parameters (List.js plugin)
 var paginationParams = {
   name: "listPages",
