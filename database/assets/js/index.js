41162b8b10b41cdfad6c6799d314541dc6bf0ff4
Peter Wang <ptrw08@gmail.com>
2016-11-18 17:51:49 -0500

    GNU General Public License v3.0

--- database/assets/js/index.js
+++ database/assets/js/index.js
@@ -1,5 +1,22 @@
 // Javascript for index page
 
+//--------------------------------------------------------------------------
+// GPL statement:
+// This file is part of RDB.
+//
+// RDB is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RDB is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with the RDB code.  If not, see <http://www.gnu.org/licenses/>.
+//--------------------------------------------------------------------------
 
 "use strict";
 
